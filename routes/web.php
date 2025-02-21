f0f4a2018db88771b2c98d958cd2a285541ed34b
Rémi Debruyne <remidebruyne@hotmail.com>
2025-02-21 16:46:45 +0100

    feat(route) : add routing

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReservationController;
+use App\Models\Reservation;
 use Illuminate\Support\Facades\Route;
+use App\Models\Course;
+use Illuminate\Support\Facades\DB;
+
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return view('home', ['courses' => Course::paginate(10)]);
 });
 
 Route::get('/dashboard', function () {
@@ -15,6 +22,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('courses/{id}/reservation', [CourseController::class, 'book']);
 });
 
-require __DIR__.'/auth.php';
+Route::resource('courses', CourseController::class)->except(['index', 'show']);
+
+Route::resource('reservations', ReservationController::class);
+
+
+
+require __DIR__ . '/auth.php';
